5f91fc53fc3d261d1a098b18a46ab75c0164586b
Rohit Tokala <rtokala@purdue.edu>
2023-05-03 12:04:15 -0400

    Add signIn signout button in header

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,8 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import Header from "~/components/header";
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
+import styles from "./styles.css?inline";
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -20,7 +18,6 @@ export default component$(() => {
       <main>
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
